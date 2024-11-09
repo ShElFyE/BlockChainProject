a347317cbedce76b70ef0a0a3efc258f98977f97
ShElFyE <daniil.komarchuk0@gmail.com>
2024-11-09 11:13:10 +0200

    added controller

---
+++ Wallet2/Wallet2/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourNamespace.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHelloWorld()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
