463e69844bcd16c56390362e9e5a349ea460b929
ShElFyE <daniil.komarchuk0@gmail.com>
2024-11-16 15:05:42 +0200

    added CRUD

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = 0;
+Console.WriteLine("Введите число:");
+n = int.Parse(Console.ReadLine());
+if (n > 1)
+{
+    for (var i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            Console.WriteLine(" не просте");
+        }
+        else
+        {
+            Console.WriteLine("просте");
+        }
+    }
+}
+else
+{
+    Console.WriteLine(" не просте");
+}
